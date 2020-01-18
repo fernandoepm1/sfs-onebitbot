b4f7221ff3f8a8cf2f80baf06d6d4e687e6867b5
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-01-18 16:16:47 -0300

    Adjust webhook endpoint

--- app.rb
+++ app.rb
@@ -6,21 +6,27 @@ require './config/database'
 
 # Load models
 Dir['./app/models/*.rb'].each { |file| require file }
+Dir['./app/services/**/*.rb'].each { |file| require file }
 
 class App < Sinatra::Base
+  before do
+    request.body.rewind
+    @request_payload = ActiveSupport::JSON.decode(request.body.read)
+  end
+
   get '/' do
     'Hello World!'
   end
 
   post '/webhook' do
-    request.body.rewind
-    result = JSON.parse(request.body.read)["queryResult"]
+    result = @request_payload['queryResult'] if @request_payload['queryResult'].present?
 
-    if result["contexts"].present?
-      response = InterpretService.call(result["action"], result["contexts"][0]["parameters"])
-    else
-      response = InterpretService.call(result["action"], result["parameters"])
-    end
+    response =
+      if result['outputContexts'].present?
+        InterpreterService.call(result['action'], result['outputContexts'][0]['parameters'])
+      else
+        InterpreterService.call(result['action'], result['parameters'])
+      end
 
     content_type :json, charset: 'utf-8'
     {
